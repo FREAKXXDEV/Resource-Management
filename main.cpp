cc26ab0526a721b7559f74a97663feb03a8f6a7c
IE <99809861+FREAKXXDEV@users.noreply.github.com>
2024-01-15 17:19:04 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <SFML/Graphics.hpp>
+#include "resourceHolder.h"
+
+const unsigned int WINDOW_WIDTH = 840;
+const unsigned int WINDOW_HEIGHT = 520;
+
+int main() {
+	sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "SFML Project", sf::Style::Close);
+	ResourcecHolder<sf::Texture, Textures::ID> textures;
+	textures.load(Textures::Asteroid, (std::string)"Resources/asteroid.png");
+	textures.load(Textures::Spaceship, (std::string)"Resources/spaceship.png");
+
+	float scale = 512.0f / 64.0f / 100.0f;
+	sf::Sprite player;
+	player.setTexture(textures.get(Textures::Spaceship));
+
+	sf::Sprite asteroid;
+	asteroid.setTexture(textures.get(Textures::Asteroid));
+#pragma region Definitions
+
+#pragma endregion
+
+	float deltaTime = 0.0f;
+	sf::Clock clock;
+
+	while (window.isOpen()) {
+		deltaTime = clock.restart().asSeconds();
+
+		sf::Event evnt;
+		while (window.pollEvent(evnt)) {
+			switch (evnt.type) {
+			case sf::Event::Closed:
+				window.close();
+				break;
+			}
+		}
+
+#pragma region Updates
+
+#pragma endregion
+
+#pragma region Visuals
+		window.clear();
+
+		window.draw(asteroid);
+		window.draw(player);
+		window.display();
+#pragma endregion
+	}
+
+	return 0;
+}
---
+++ resourceHolder.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <map>
+#include <memory>
+#include <SFML/Graphics.hpp>
+
+namespace Textures { // I am only providing a "texture"-based enum but you can implement more
+	enum ID { Spaceship, Asteroid };
+}
+
+template <typename Resource, typename Identifier>
+class ResourcecHolder {
+public:
+	void load(Identifier id, const std::string &fileName); // resource loader
+	Resource &get(Identifier id); // resourcec access
+	const Resource &get(Identifier id) const;
+
+private:
+	std::map<Identifier, std::unique_ptr<Resource>> resourceMap; // resource container
+};
+
+template<typename Resource, typename Identifier>
+inline void ResourcecHolder<Resource, Identifier>::load(Identifier id, const std::string &fileName) {
+	std::unique_ptr<Resource> resource(new Resource); // creating a smart pointer for a new texture
+	resource->loadFromFile(fileName); // loading the reource 
+
+	resourceMap.insert(std::make_pair(id, std::move(resource))); // appending the resource with an id into the map
+}
+
+template<typename Resource, typename Identifier>
+inline Resource &ResourcecHolder<Resource, Identifier>::get(Identifier id) {
+	auto found = resourceMap.find(id); // getting a pointer to the resource from the map
+	return *found->second; 
+}
+
+template<typename Resource, typename Identifier>
+inline const Resource &ResourcecHolder<Resource, Identifier>::get(Identifier id) const {
+	auto found = resourceMap.find(id); // basically the same
+	return *found->second;
+}
